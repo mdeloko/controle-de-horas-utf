9728f85df990a4e182d82981a9dce0fe1113caaa
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2026-05-09 19:22:32 -0300

    feat(auth): implementar serviço, controller e rota de login/logout

---
+++ backend/src/routes/authRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import * as authController from '../controllers/authController.js'
+
+const authRouter = Router()
+
+authRouter.post('/login', authController.login)
+authRouter.post('/logout', authController.logout)
+
+export default authRouter
