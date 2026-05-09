938e0723ec6a8037e6a79d56ffc16f5662a3f4bc
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2026-05-09 19:32:24 -0300

    feat(server): registrar autenticação e rotas de usuários no app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import fs from "fs";
 import seed from "./database/seed.js";
 import db from "./database/database.js";
 import userRouter from "./routes/userRoutes.js";
+import authRouter from "./routes/authRoutes.js";
+import authMiddleware from "./middlewares/authMiddleware.js";
 import tipoAtividadeRouter from "./routes/tipoAtividadeRoutes.js";
 import logger from "./middlewares/logger.js";
 
@@ -18,12 +20,20 @@ if(!pathExists){
 
 process.env.NODE_ENV === 'development' && app.use(logger);
 app.use(express.json());
-app.use('/user',userRouter);
+
 app.use('/tipos-atividade', tipoAtividadeRouter);
+
+// rotas públicas
+app.use('/auth', authRouter);
 
 app.get("/ping",(_,res)=>{
     const data:string = new Date().toLocaleDateString("pt-BR",{hour:"numeric",minute:"numeric",second:"numeric"})
     res.json({msg:"Pong!",time:data}).status(200)
 })
+
+app.use(authMiddleware);
+
+// rotas protegidas
+app.use('/user', userRouter);
 
 app.listen(PORT,() => console.log('🚀 Servidor funcionando na porta', PORT));
